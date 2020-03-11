c86722534713f16bd31f23ed82cdf05668cf0827
dominiquesydow <dominique.sydow@posteo.de>
2020-03-11 16:07:48 +0100

    Rename module: information to kinases

--- klifs_utils/remote/kinases/__init__.py
+++ klifs_utils/remote/kinases/__init__.py
@@ -4,4 +4,4 @@ Utility functions to work with KLIFS data
 """
 
 from .select import kinase_groups, kinase_families, kinase_names, kinase_ids
-from .load import from_kinase_id, from_kinase_name
+from .details import from_kinase_id, from_kinase_name
