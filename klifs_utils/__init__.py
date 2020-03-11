c7f9ae720e1eb6b88892b93266b5fb3eaefc12da
dominiquesydow <dominique.sydow@posteo.de>
2020-03-11 18:18:16 +0100

    Add util func: mol2 (file/text) to dataframe/rdkit mol

--- klifs_utils/__init__.py
+++ klifs_utils/__init__.py
@@ -8,6 +8,8 @@ from .klifs_utils import *
 from . import local
 from . import remote
 from .util import abc_idlist_to_dataframe
+from .util import mol2_text_to_dataframe, mol2_file_to_dataframe
+from .util import mol2_text_to_rdkit_mol, mol2_file_to_rdkit_mol
 
 # Handle versioneer
 from ._version import get_versions
--- klifs_utils/util.py
+++ klifs_utils/util.py
@@ -5,6 +5,7 @@ Utility functions to work with KLIFS data
 General utility functions
 """
 
+from biopandas.mol2 import PandasMol2
 import pandas as pd
 
 
@@ -33,3 +34,44 @@ def abc_idlist_to_dataframe(abc_idlist):
             results_dict[key].append(result[key])
 
     return pd.DataFrame(results_dict)
+
+
+def mol2_file_to_dataframe(mol2_file):
+    pass
+
+
+def mol2_text_to_dataframe(mol2_text):
+    """
+    Get KLIFS residue names and IDs.
+
+    Parameters
+    ----------
+    mol2_text : str
+       Mol2 file content from KLIFS database.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Table of mol2 file residue names and IDs.
+    """
+
+    ppdb = PandasMol2()
+
+    mol2_df = ppdb._construct_df(
+        mol2_text.splitlines(True),
+        # TODO: Rename 'Residues' to 'subst_name'
+        col_names=['atom_id', 'atom_name', 'x', 'y', 'z', 'atom_type', 'subst_id', 'subst_name', 'charge', 'backbone'],
+        col_types=[int, str, float, float, float, str, int, str, float, str]
+
+    )
+
+    return mol2_df
+
+
+def mol2_file_to_rdkit_mol(mol2_file):
+    pass
+
+
+
+def mol2_text_to_rdkit_mol(mol2_text):
+    pass
