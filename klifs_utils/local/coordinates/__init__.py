f6dacd0290f846ae9e7917afcf25c7977d5a27bc
dominiquesydow <dominique.sydow@posteo.de>
2020-03-13 11:25:15 +0100

    Remove unneccessary imports from local/coordinates/__init__.py

--- klifs_utils/local/coordinates/__init__.py
+++ klifs_utils/local/coordinates/__init__.py
@@ -6,8 +6,8 @@ Coordinates.
 """
 
 # Add imports of sub-modules here
-from .complex import mol2_to_dataframe
-from .protein import mol2_to_dataframe
-from .pocket import mol2_to_dataframe
-from .ligand import mol2_to_dataframe, mol2_to_rdkit_mol
-from .water import mol2_to_dataframe
+from . import complex
+from . import protein
+from . import pocket
+from . import ligand
+from . import water
