07ed07845d0c02605b1787ce760531eaa9f19b69
dominiquesydow <dominique.sydow@posteo.de>
2020-03-13 11:24:12 +0100

    Remove unneccessary imports from top-level __init__.py

--- klifs_utils/__init__.py
+++ klifs_utils/__init__.py
@@ -4,9 +4,7 @@ Utility functions to work with KLIFS data
 """
 
 # Add imports here
-from .klifs_utils import *
 from . import remote, local
-from .util import _abc_idlist_to_dataframe
 
 # Handle versioneer
 from ._version import get_versions
