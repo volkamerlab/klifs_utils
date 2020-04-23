b9640097baeb747dcfaf0b7dd4ffc4c415426f56
dominiquesydow <dominique.sydow@posteo.de>
2020-04-23 09:52:29 +0200

    Structures module: Allow for alt/chain specific requests

--- klifs_utils/remote/structures.py
+++ klifs_utils/remote/structures.py
@@ -28,8 +28,9 @@ def structures_from_structure_id(structure_ids):
         structure_ids = [structure_ids]
 
     result = KLIFS_CLIENT.Structures.get_structure_list(structure_ID=structure_ids).response().result
+    result_df = _abc_idlist_to_dataframe(result)
 
-    return _abc_idlist_to_dataframe(result)
+    return result_df
 
 
 def structures_from_kinase_id(kinase_ids):
@@ -51,18 +52,23 @@ def structures_from_kinase_id(kinase_ids):
         kinase_ids = [kinase_ids]
 
     result = KLIFS_CLIENT.Structures.get_structures_list(kinase_ID=kinase_ids).response().result
+    result_df = _abc_idlist_to_dataframe(result)
 
-    return _abc_idlist_to_dataframe(result)
+    return result_df
 
 
-def structures_from_pdb_id(pdb_ids):
+def structures_from_pdb_id(pdb_ids, alt=None, chain=None):
     """
-    Get structure details by PDB ID(s).
+    Get structure details by PDB ID(s), optionally filter by alternate model and chain.
 
     Parameters
     ----------
     pdb_ids : str or list of str
         PDB ID(s).
+    alt : None or str
+        Alternate model.
+    chain : None or str
+        Chain.
 
     Returns
     -------
@@ -74,5 +80,14 @@ def structures_from_pdb_id(pdb_ids):
         pdb_ids = [pdb_ids]
 
     result = KLIFS_CLIENT.Structures.get_structures_pdb_list(pdb_codes=pdb_ids).response().result
+    result_df = _abc_idlist_to_dataframe(result)
 
-    return _abc_idlist_to_dataframe(result)
+    # If alt and/or chain are given, filter dataset, else return full dataset
+    if alt is not None:
+        return result_df[result_df.alt == alt]
+    elif chain is not None:
+        return result_df[result_df.chain == chain]
+    elif alt is not None and chain is not None:
+        return result_df[(result_df.alt == alt) and (result_df.chain == chain)]
+    else:
+        return result_df
