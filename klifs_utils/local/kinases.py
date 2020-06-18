b212be05d60323069017465186f3a3748c3f729a
Unknown <dominique.sydow@posteo.de>
2020-06-18 15:32:57 +0200

    Add code to local.kinases.kinase_names

--- klifs_utils/local/kinases.py
+++ klifs_utils/local/kinases.py
@@ -44,19 +44,10 @@ def kinase_families(klifs_metadata, kinase_group=None):
     return kinase_families
 
 
-def kinase_names(klifs_metadata, kinase_group=None, kinase_family=None, species=None):
+def kinase_names(klifs_metadata):
     """
     Get all kinase names for kinases belonging to a given kinase group, kinase family and/or species (default is None,
     i.e. get all kinase names). If multiple parameters are set, only kinases fullfilling all conditions are returned.
-
-    Parameters
-    ----------
-    kinase_group : None or str
-        Kinase group name (default is None, i.e. all kinase groups are selected).
-    kinase_family : None or str
-        Kinase family name (default is None, i.e. all kinase families are selected).
-    species : None or str
-        Species name (default is None, i.e. all species are selected).
 
     Returns
     -------
@@ -64,16 +55,18 @@ def kinase_names(klifs_metadata, kinase_group=None, kinase_family=None, species=
         Kinase names with details.
     """
 
-    return
+    klifs_metadata = klifs_metadata.drop_duplicates(subset=['kinase', 'species'])[['kinase', 'species']]
+
+    return klifs_metadata
 
 
-def kinase_from_kinase_name(klifs_metadata, kinase_name, species=None):
+def kinase_from_kinase_names(klifs_metadata, kinase_names, species=None):
     """
     Get all kinases (+details) by kinase name(s).
 
     Parameters
     ----------
-    kinase_name : str
+    kinase_names : str or list of str
         Kinase name.
     species : None or str
         Species name (default is None, i.e. all species are selected).
