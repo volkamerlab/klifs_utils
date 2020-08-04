ad76f31de89cd8bf5e5e099de3913d51bd605695
dominiquesydow <dominique.sydow@posteo.de>
2020-08-04 14:58:14 +0200

    Simplify coordinate module (less redundancy)

--- klifs_utils/util.py
+++ klifs_utils/util.py
@@ -13,6 +13,88 @@ import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+COORDINATES_ENTITIES = ['complex', 'ligand', 'pocket', 'protein', 'water']
+COORDINATES_INPUT_FORMATS = ['mol2', 'pdb']
+COORDINATES_OUTPUT_FORMATS = ['text', 'biopandas', 'rdkit']
+
+
+def file_path(path, species, kinase, pdb, alt, chain, entity='complex', format='mol2', in_dir=False):
+    """
+    Get file path.
+
+    Parameters
+    ----------
+    path : pathlib.Path or str
+        Path to folder for file destination (if in_dir=False) or KLIFS_downlaod folder destination (if in_dir=True).
+    species : str
+        Species.
+    kinase : str
+        Kinase name.
+    pdb : str
+        PDB ID.
+    alt : str
+        Alternate model ID.
+    chain : str
+        Chain ID.
+    entitiy : str
+        Structural entity: complex (default), ligand, pocket, protein, or water (only in local module).
+    format : str
+        File format: mol2 (default) or pdb (only for entity=complex).
+    in_dir : bool
+        Use KLIFS directory structure (default: False).
+
+    Returns
+    -------
+    pathlib.Path
+        File path.
+    """
+
+    structure = f"{pdb}{f'_alt{alt}' if bool(alt) else ''}{f'_chain{chain}' if bool(chain) else ''}"
+
+    if in_dir:
+        path = path / 'KLIFS_download' / species / kinase / structure / f'{entity}.{format}'
+    else:
+        path = path / f'{species}_{kinase}_{structure}_{entity}.{format}'
+
+    return path
+
+
+def check_entity_format(entity, input_format, output_format=None):
+    """
+    Check if entity and format (and their combinations) are available.
+
+    Parameters
+    ----------
+    entity : str
+        Structural entity: complex, ligand, pocket, protein, or water (only in local module).
+    input_format : str
+        File input format: mol2 or pdb (only for entity=complex).
+    output_format : str or None
+        Output format: text (only in remote module), biopandas, or rdkit (only for entity=ligand).
+    """
+
+    entities = COORDINATES_ENTITIES.copy()
+    # No remote access to water, thus remove from list
+    entities.remove('water')
+    input_formats = COORDINATES_INPUT_FORMATS
+    output_formats = COORDINATES_OUTPUT_FORMATS
+
+    # Check if parameters are valid
+    if entity not in entities:
+        raise ValueError(f'Invalid entity. Select from {", ".join(entities)}.')
+    if input_format not in input_formats:
+        raise ValueError(f'Invalid input format. Select from {", ".join(input_formats)}.')
+    if output_format:
+        if output_format not in output_formats:
+            raise ValueError(f'Invalid output format. Select from {", ".join(output_formats)}.')
+
+    # Check if parameter combination is valid
+    if input_format == 'pdb' and entity != 'complex':
+        raise ValueError(f'Entity {entity} is only available in mol2 format.')
+    if output_format:
+        if output_format == 'rdkit' and entity != 'ligand':
+            raise ValueError(f'Only entity ligand can be fetched as rdkit molecule.')
+
 
 def _abc_idlist_to_dataframe(abc_idlist):
     """
@@ -40,224 +122,3 @@ def _abc_idlist_to_dataframe(abc_idlist):
             results_dict[key].append(result[key])
 
     return pd.DataFrame(results_dict)
-
-
-def _mol2_path(structure_type, klifs_download_path, species, kinase_name, pdb_id, alt, chain):
-    """
-    Get mol2 file path.
-
-    Parameters
-    ----------
-    structure_type : str
-        Structure type, i.e. complex, protein, pocket, ligand, or water.
-    klifs_download_path : pathlib.Path or str
-        Path to folder where KLIFS_download folder lives.
-    species : str
-        Species.
-    kinase_name : str
-        Kinase name.
-    pdb_id : str
-        PDB ID.
-    alt : str
-        Alternate model ID.
-    chain : str
-        Chain ID.
-
-    Raises
-    ------
-    ValueError
-        If structure type invalid.
-    FileNotFoundError
-        If file path does not exist.
-
-
-    Returns
-    -------
-    pathlib.Path
-        Path to mol2 path.
-    """
-
-    structure_types = 'complex protein pocket ligand water'.split()
-
-    if structure_type not in structure_types:
-        raise ValueError(f'Invalid structure type {structure_type}. Choose from {",".join(structure_types)}.')
-
-    klifs_download_path = Path(klifs_download_path)
-    species = species.upper()
-
-    if alt and chain:
-        structure_name = f'{pdb_id}_alt{alt}_chain{chain}'
-    elif alt and not chain:
-        structure_name = f'{pdb_id}_alt{alt}'
-    elif not alt and chain:
-        structure_name = f'{pdb_id}_chain{chain}'
-    else:
-        structure_name = f'{pdb_id}'
-
-    mol2_path = klifs_download_path / species / kinase_name / structure_name / f'{structure_type}.mol2'
-
-    if not mol2_path.exists():
-        raise FileNotFoundError(f'File not found: {mol2_path}.')
-
-    return mol2_path
-
-
-def _mol2_file_to_dataframe(mol2_file):
-    """
-    Get structural data from mol2 file.
-
-    Parameters
-    ----------
-    mol2_file : pathlib.Path or str
-       Path to mol2 file.
-
-    Returns
-    -------
-    pandas.DataFrame
-        Structural data.
-    """
-
-    mol2_file = Path(mol2_file)
-
-    pmol = PandasMol2()
-
-    try:
-        mol2_df = pmol.read_mol2(
-            str(mol2_file),
-            columns={
-                0: ('atom_id', int),
-                1: ('atom_name', str),
-                2: ('x', float),
-                3: ('y', float),
-                4: ('z', float),
-                5: ('atom_type', str),
-                6: ('subst_id', int),
-                7: ('subst_name', str),
-                8: ('charge', float),
-                9: ('backbone', str)
-            }
-        )
-
-    except ValueError:
-        mol2_df = pmol.read_mol2(
-            str(mol2_file)
-        )
-
-    return mol2_df
-
-
-def _mol2_file_to_rdkit_mol(mol2_file, compute2d=True):
-    """
-    Get structural data from mol2 file.
-
-    Parameters
-    ----------
-    mol2_file : pathlib.Path or str
-       Path to mol2 file.
-    compute2d : bool
-        Compute 2D coordinates for ligand (default).
-
-    Returns
-    -------
-    rdkit.Chem.rdchem.Mol
-        Molecule.
-    """
-
-    mol = Chem.MolFromMol2File(mol2_file)
-
-    if compute2d:
-        AllChem.Compute2DCoords(mol)
-
-    return mol
-
-
-def _mol2_text_to_dataframe(mol2_text):
-    """
-    Get structural data from mol2 text.
-
-    Parameters
-    ----------
-    mol2_text : str
-       Mol2 file content from KLIFS database.
-
-    Returns
-    -------
-    pandas.DataFrame
-        Structural data.
-    """
-
-    pmol = PandasMol2()
-
-    try:
-        mol2_df = pmol._construct_df(
-            mol2_text.splitlines(True),
-            col_names=[
-                'atom_id', 'atom_name', 'x', 'y', 'z', 'atom_type', 'subst_id', 'subst_name', 'charge', 'backbone'
-            ],
-            col_types=[
-                int, str, float, float, float, str, int, str, float, str
-            ]
-        )
-    except ValueError:
-        mol2_df = pmol._construct_df(
-            mol2_text.splitlines(True),
-            col_names=[
-                'atom_id', 'atom_name', 'x', 'y', 'z', 'atom_type', 'subst_id', 'subst_name', 'charge'
-            ],
-            col_types=[
-                int, str, float, float, float, str, int, str, float
-            ]
-        )
-
-    return mol2_df
-
-
-def _mol2_text_to_rdkit_mol(mol2_text, compute2d=True):
-    """
-    Get structural data from mol2 text.
-
-    Parameters
-    ----------
-    mol2_text : str
-       Mol2 file content from KLIFS database.
-    compute2d : bool
-        Compute 2D coordinates for ligand (default).
-
-    Returns
-    -------
-    rdkit.Chem.rdchem.Mol
-        Molecule.
-    """
-
-    mol = Chem.MolFromMol2Block(mol2_text)
-
-    if compute2d:
-        AllChem.Compute2DCoords(mol)
-
-    return mol
-
-
-def _pdb_text_to_dataframe(pdb_text):
-    """
-    Get structural data from pdb text.
-
-    Parameters
-    ----------
-    pdb_text : str
-       Pdb file content from KLIFS database.
-
-    Returns
-    -------
-    dict of pandas.DataFrame
-        Structural data
-    """
-
-    ppdb = PandasPdb()
-
-    pdb_dict = ppdb._construct_df(
-        pdb_text.splitlines(True)
-    )
-
-    print(f'Structural data keys: {pdb_dict.keys()}')
-
-    return pdb_dict
